33c86ea48d98ce50c8cdc4bf251a5c064cedf520
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-11-19 12:01:37 +0100

    Architettura componenti pronta

---
+++ src/app/recipes/recipe-list/recipe-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RecipeItem } from "./recipe-item/recipe-item";
+
+@Component({
+  selector: 'app-recipe-list',
+  imports: [RecipeItem],
+  templateUrl: './recipe-list.html',
+  styleUrl: './recipe-list.css',
+})
+export class RecipeList {
+
+}
---
+++ src/app/recipes/recipes.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RecipeList } from "./recipe-list/recipe-list";
+import { RecipeDetail } from "./recipe-detail/recipe-detail";
+
+@Component({
+  selector: 'app-recipes',
+  imports: [RecipeList, RecipeDetail],
+  templateUrl: './recipes.html',
+  styleUrl: './recipes.css',
+})
+export class Recipes {
+
+}
