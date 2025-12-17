020b754784889ca12b81fc6e1095dd437fafc25b
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-12-17 12:15:58 +0100

    Gestita apertura/chiusura dropdown su RecipeDetail

--- src/app/recipes/recipe-detail/recipe-detail.ts
+++ src/app/recipes/recipe-detail/recipe-detail.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { RecipeModel } from '../../models/recipe.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recipe-detail.html',
   styleUrl: './recipe-detail.css',
 })
 export class RecipeDetail {
   @Input() recipe: RecipeModel | undefined
-  
+
+  isDropdownOpen: boolean = false;
 }
